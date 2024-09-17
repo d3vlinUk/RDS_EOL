f6a9ab79ea8616ff50de4eacd00d7e951d7747e4
Rob Wilson <110221911+d3vlinUk@users.noreply.github.com>
2024-09-17 15:50:18 +0100

    Update RDSPostgresDates.py

--- GenerateDates/RDSPostgresDates.py
+++ GenerateDates/RDSPostgresDates.py
@@ -7,6 +7,11 @@ from datetime import datetime
 
 LAMBDA_DATES = "https://docs.aws.amazon.com/lambda/latest/dg/lambda-runtimes.html"
 RDS_PG_DATES = "https://docs.aws.amazon.com/AmazonRDS/latest/PostgreSQLReleaseNotes/postgresql-release-calendar.html"
+RDS_MYSQL_DATES = "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/MySQL.Concepts.VersionMgmt.html"
+RDS_MARIA_DATES = "https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/MariaDB.Concepts.VersionMgmt.html"
+EKS_DATES = "https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html"
+NEPTUNE_DATES = "https://docs.aws.amazon.com/neptune/latest/userguide/engine-releases.html"
+
 JSON_FILE_NAME = "../Dates/AWS_Eol_Dates.json"
 
 
@@ -44,7 +49,7 @@ def convertDate(input):
         if (is_date_parsing(input)):
             date = date_parser.parse(input).date()
             return date.strftime('%b %d, %Y')
-    return input
+    return None
 
 def isStringEmpty(string):
     if(len(string) == 0):
@@ -62,16 +67,15 @@ def getDates(params = {}):
         if len(cells) > 3:
             for index in params['TableIndex']:
                 # Ignore the data if it's empty/not present
-                if isStringEmpty(cells[3].text.strip()) == False:
                     string = removeCharacters(cells[index].text.strip())
                     match index:
                         case value if value == params['TableIndex'][0]:
-                            headerIndex = 'Version'
+                            headerIndex = params['Type']+'.Version'
                         case value if value == params['TableIndex'][1]:
                             headerIndex = 'Date'
                             string = convertDate(string)
                     items[headerIndex] = string
-        if items:
+        if items and items['Date'] is not None:
             data.append(items)
 
     return data
@@ -79,8 +83,25 @@ def getDates(params = {}):
 if __name__ == "__main__":
     
     jsondata = {}
-    jsondata['lambda'] = getDates({'DatesUrl':LAMBDA_DATES,'TableId':0,'TableIndex':[1,3]})
-    jsondata['RDS_PG'] = getDates({'DatesUrl':RDS_PG_DATES,'TableId':1,'TableIndex':[0,3]})
+    # Databases
+    jsondata['RDS_PG'] = getDates({'Type':'RDS_pg','DatesUrl':RDS_PG_DATES,'TableId':1,'TableIndex':[0,3]})
+    jsondata['RDS_MYSQL'] = getDates({'Type':'RDS_MYSQL','DatesUrl':RDS_MYSQL_DATES,'TableId':0,'TableIndex':[0,3]})
+    
+    #MariaDB
+    mariaData = []
+    for i in range(4):
+        mariaData = mariaData + getDates({'Type':'RDS_MARIA','DatesUrl':RDS_MARIA_DATES,'TableId':i,'TableIndex':[0,3]})
+    jsondata['RDS_MARIA'] = mariaData
+    
+    #Neptune
+    neptuneData = []
+    neptuneData = neptuneData + getDates({'Type':'Neptune','DatesUrl':NEPTUNE_DATES,'TableId':0,'TableIndex':[0,6]})
+    neptuneData = neptuneData + getDates({'Type':'Neptune','DatesUrl':NEPTUNE_DATES,'TableId':1,'TableIndex':[0,5]})
+    jsondata['Neptune'] = neptuneData
+    
+    # Compute
+    jsondata['lambda'] = getDates({'Type':'Lambda','DatesUrl':LAMBDA_DATES,'TableId':0,'TableIndex':[1,3]})
+    jsondata['EKS'] = getDates({'Type':'EKS','DatesUrl':EKS_DATES,'TableId':0,'TableIndex':[0,3]})
     
     print(json.dumps(jsondata, indent=2))
     print(json.dumps(jsondata, indent=2),file=open(JSON_FILE_NAME, 'w'))
